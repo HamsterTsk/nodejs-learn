459419349727d0f8721719cb8841ca1811ed5ff5
HamsterTsk <hamstertomsk70@gmail.com>
2019-06-19 11:12:37 +0700

    chapter 4. create lib directory for our modules

--- meadowlark.js
+++ meadowlark.js
@@ -1,40 +1,33 @@
+const fortune = require('./lib/fortune');
 const express = require('express');
+const handlebars = require('express-handlebars').create({ defaultLayout:'main' });
 const app = express();
-const handlebars = require('express-handlebars')
-    .create({ defaultLayout:'main' });
-let fortunes = [
-    "Победи свои страхи, или они победят тебя.",
-    "Рекам нужны истоки.",
-    "Не бойся неведомого.",
-    "Тебя ждет приятный сюрприз.",
-    "Будь проще везде, где только можно.",
-];
+
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
 app.set('port', process.env.PORT || 3000);
-
 app.use(express.static(__dirname + '/public'));
+
 
 app.get('/' , function (req, res) {
     res.render('home');
 });
 app.get('/about', function (req, res) {
-    let randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getFortune()});
 });
+
 
 app.use(function (req, res, next) {
     res.status(404);
     res.render('404')
 });
-
 app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500);
     res.render('500')
 });
+
 
 app.listen(app.get('port'), function () {
     console.log('Express запущен на http://localhost:' + app.get('port') + ': нажмите Ctrl+C Для завершения.');
